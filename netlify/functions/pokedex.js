ff6d086a4eb6dc5a901afda10ad66d16f86937f9
Ben Hong <ben@bencodezen.io>
2021-07-27 16:45:29 -0400

    feature: add new pokedex api endpoint

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,11 @@
+exports.handler = async function () {
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+  const response = await fetch(POKE_API)
+  const data = await response.json()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+}
