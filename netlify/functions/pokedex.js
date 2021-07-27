b24daf93acd7f9236d99aa4baa17da70403ff457
Ben Hong <ben@bencodezen.io>
2021-07-27 16:53:40 -0400

    refactor: use a POST method to specify kanto region

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch')
 
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
   const response = await fetch(POKE_API)
   const data = await response.json()
